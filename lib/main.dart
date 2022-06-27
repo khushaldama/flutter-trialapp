14edce769bcbfcff4993065afbecc9496a3cf76d
KHUSHAL DHIRAJBHAI BHANUSHALI <100462293+khushaldama@users.noreply.github.com>
2022-06-27 14:53:26 +0530

    ch2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Flutter First App",
     home: HomePage(),
   ));
-
 }
 
 class HomePage extends StatelessWidget {
@@ -15,12 +14,59 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter App"),
-      ),
-      body: Container(
-        child: Center(child: Text("This the body part of flutter application")),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Flutter App"),
+        ),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  //padding: const EdgeInsets.all(12),
+                  alignment: Alignment.center,
+                  //color: Colors.yellow,
+                  margin: const EdgeInsets.all(5),
+                  width: 110,
+                  height: 110,
+
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 5,
+                            spreadRadius: 2,
+                            offset: Offset(2.0, 5.0))
+                      ],
+                      //color: Colors.orangeAccent,
+                      gradient: LinearGradient(colors: [
+                        Colors.orange,
+                        Colors.white,
+                        Colors.green,
+                      ])),
+                  child: Text("This is trial box",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 10)),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(5),
+                  width: 110,
+                  height: 110,
+                  color: Colors.pink,
+                ),
+                Container(
+                  margin: const EdgeInsets.all(5),
+                  width: 110,
+                  height: 110,
+                  color: Colors.lightGreen,
+                )
+              ]),
+        ));
   }
 }
